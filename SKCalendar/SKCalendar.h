9fb390543949b659352e6c3866f15df9fb0b83de
Sandeep <sandeep.koirala@beddit.com>
2015-09-26 12:51:44 +0300

    Initial Commit

---
+++ SKCalendar/SKCalendar.h
@@ -0,0 +1,19 @@
+//
+//  SKCalendar.h
+//  SKCalendar
+//
+//  Created by Sandeep Koirala on 26/09/15.
+//  Copyright © 2015 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKCalendar.
+FOUNDATION_EXPORT double SKCalendarVersionNumber;
+
+//! Project version string for SKCalendar.
+FOUNDATION_EXPORT const unsigned char SKCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKCalendar/PublicHeader.h>
+
+
